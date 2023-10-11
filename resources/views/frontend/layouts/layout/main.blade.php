0485d9f149f6003fed17f8b7d9da326fd252e1ec
KhemJiAimaim <119541041+KhemJiAimaim@users.noreply.github.com>
2023-10-11 15:06:53 +0700

    gg

--- resources/views/frontend/layouts/layout/main.blade.php
+++ resources/views/frontend/layouts/layout/main.blade.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="resources/css/app.css">
     @vite('resources/css/app.css')
     <title>Document</title>
+
 </head>
 
 <body>
     @include('frontend.layouts.layout.head')
     @include('frontend.layouts.layout.banner')
-    <div class="py-32 text-center">
+    <div class="text-center">
         @yield('content')
     </div>
 
