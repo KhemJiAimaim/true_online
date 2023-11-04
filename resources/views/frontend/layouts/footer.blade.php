ae1e70d0648594a6a16c3881957a9f22f541290f
KhemJiAimaim <119541041+KhemJiAimaim@users.noreply.github.com>
2023-11-04 09:34:38 +0700

    home

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,28 +1,28 @@
 <div class=" bg-[#211830] sticky top-[100vh]">
     <div class=" max-w-[1536px] mx-auto">
 
-        <div class="flex sm:justify-between flex-wrap py-4 items-center gap-y-3">
-            <div class="sm:grid sm:grid-row-2 sm:gap-[0] sm:mx-0 flex justify-between gap-[7.3rem] mx-auto">
+        <div class="flex flex-wrap py-4  gap-y-3">
+            <div class="2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem] mx-auto">
                 <p class="text-white sm:text-lg text-md ">สมัครบริการใหม่</p>
                 <p class="text-white sm:text-2xl text-md ">000-000-0000</p>
             </div>
             @if (Request::is('/') || Request::is('prepaid_sim'))
-                <div class="flex sm:justify-between gap-6 mx-auto items-center">
+                <div class="  md:flex lg:flex 2xl:flex md:justify-between lg:justify-between 2xl:justify-between gap-6 mx-auto items-left grid grid-rows-3 gap-y-2">
                     <p class="text-white sm:text-lg text-md text-center">ความปลอดภัย</p>
                     <p class="text-white sm:text-lg text-md text-center">ความเป็นส่วนตัว</p>
                     <p class="text-white sm:text-lg text-md text-center">ปัญหาร้องเรียน</p>
                 </div>
             @endif
             @if (Request::is('/') || Request::is('prepaid_sim') )
-                <div class="sm:grid sm:grid-row-2 sm:gap-[0] sm:mx-0 flex justify-between gap-[12rem] mx-auto">
-                    <p class="text-white sm:text-lg text-md sm:text-right">แก้ไขปัญหา โทร</p>
-                    <p class="text-white sm:text-2xl text-md sm:text-right">1242</p>
+                <div class="s2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem] mx-auto">
+                    <p class="text-white sm:text-lg text-md text-right">แก้ไขปัญหา โทร</p>
+                    <p class="text-white sm:text-2xl text-md text-right">1242</p>
                 </div>
             @endif
             @if (Request::path() != '/' && Request::path() != 'prepaid_sim')
-                <div class="sm:grid sm:grid-row-2 sm:gap-[0] sm:mx-0 flex justify-between gap-[12rem] mx-auto">
-                    <p class="text-white sm:text-lg text-md sm:text-right">ไอดี ไลน์</p>
-                    <p class="text-white sm:text-2xl text-md sm:text-right">@True Online</p>
+                <div class="2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem] mx-auto">
+                    <p class="text-white sm:text-lg text-md 2xl:text-right">ไอดี ไลน์</p>
+                    <p class="text-white sm:text-2xl text-md text-right">@True Online</p>
                 </div>
             @endif
 
@@ -30,7 +30,7 @@
 
 
         <div class="flex justify-center border-l border border-gray-500 text-center mx-3 rounded-full px-auto"></div>
-        <div class="flex sm:justify-between justify-center flex-wrap gap-y-2 ">
+        <div class="flex sm:justify-between justify-center flex-wrap gap-y-2 px-2">
             <p class="text-white sm:text-md opacity-50 py-2 text-xs">©True Corporation Public Company Limited. All
                 rights reserved.
             </p>
