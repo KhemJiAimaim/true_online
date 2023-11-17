9fea4ee189f3bbb542a620c7037294ed9f1b732c
KhemJiAimaim <119541041+KhemJiAimaim@users.noreply.github.com>
2023-11-17 17:26:12 +0700

    update home

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -3,26 +3,26 @@
 
         <div class="flex justify-between py-4 px-4 gap-y-3">
             <div class="2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem]">
-                <p class="text-white sm:text-lg text-md ">สมัครบริการใหม่</p>
+                <p class="text-white sm:text-lg text-md 2xl:text-[20px]">สมัครบริการใหม่</p>
                 <p class="text-white sm:text-2xl text-md ">000-000-0000</p>
             </div>
 
             @if (Request::is('/') || Request::is('prepaid_sim'))
                 <div class="  md:flex lg:flex 2xl:flex md:justify-between lg:justify-between 2xl:justify-between gap-6 items-left grid grid-rows-3 gap-y-2">
-                    <p class="text-white sm:text-lg text-md text-center">ความปลอดภัย</p>
-                    <p class="text-white sm:text-lg text-md text-center">ความเป็นส่วนตัว</p>
-                    <p class="text-white sm:text-lg text-md text-center">ปัญหาร้องเรียน</p>
+                    <p class="text-white sm:text-lg text-md 2xl:text-[20px] text-center">ความปลอดภัย</p>
+                    <p class="text-white sm:text-lg text-md 2xl:text-[20px] text-center">ความเป็นส่วนตัว</p>
+                    <p class="text-white sm:text-lg text-md 2xl:text-[20px] text-center">ปัญหาร้องเรียน</p>
                 </div>
             @endif
 
             @if (Request::is('/') || Request::is('prepaid_sim') )
                 <div class="s2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem] ">
-                    <p class="text-white sm:text-lg text-md text-right">แก้ไขปัญหา โทร</p>
+                    <p class="text-white sm:text-lg text-md text-right 2xl:text-[20px]">แก้ไขปัญหา โทร</p>
                     <p class="text-white sm:text-2xl text-md text-right">1242</p>
                 </div>
             @elseif (Request::path() != '/' && Request::path() != 'prepaid_sim')
                 <div class="2xl:grid 2xl:grid-rows-2 2xl:gap-[0] 2xl:mx-0 gap-[7.3rem]">
-                    <p class="text-white sm:text-lg text-md text-right">ไอดี ไลน์</p>
+                    <p class="text-white sm:text-lg text-md text-right 2xl:text-[20px]">ไอดี ไลน์</p>
                     <p class="text-white sm:text-2xl text-md text-right">@True Online</p>
                 </div>
             @endif
@@ -32,7 +32,7 @@
 
         <div class="flex justify-center border-l border border-gray-500 text-center mx-3 rounded-full px-auto"></div>
         <div class="flex sm:justify-between items-center max-md:flex-col gap-y-2 px-2">
-            <p class="text-white sm:text-md opacity-50 py-2 text-xs">©True Corporation Public Company Limited. All
+            <p class="text-white opacity-50 py-2 2xl:text-[16px] md:text-[16px] text-[0.8rem] text-center">©True Corporation Public Company Limited. All
                 rights reserved.
             </p>
 
