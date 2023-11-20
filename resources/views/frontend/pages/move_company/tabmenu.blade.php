b694f596d0e6e7e0b377af727426c8160865e567
Sawt-07 <97818855+Sawt-07@users.noreply.github.com>
2023-11-20 17:22:43 +0700

    move5

--- resources/views/frontend/pages/move_company/tabmenu.blade.php
+++ resources/views/frontend/pages/move_company/tabmenu.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="grid  max-w-[1530px] w-[100%] 2xl:grid-cols-4 md:grid-cols-4 grid-cols-2 mt-[4rem] mx-auto items-center gap-3 ">
+    class="  max-w-[1530px] w-[100%] flex 2xl:flex-row lg:flex-row md:flex-row justify-center flex-col  mt-[4rem] mx-auto items-center gap-3 ">
     <div class=" flex justify-center">
         <a class="w-[150px] h-[40px] bg-gray-400 hover:bg-gradient-to-r from-[#F6911D] to-[#ED4312] flex items-center justify-center rounded-[5px] "
             href="/move">
