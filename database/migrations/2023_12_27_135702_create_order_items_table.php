97f7ea43c14918499402911812dc3acea19f615f
bnrboyy <nantachai.ru@gmail.com>
2023-12-27 16:54:36 +0700

    'update'

--- database/migrations/2023_12_27_135702_create_order_items_table.php
+++ database/migrations/2023_12_27_135702_create_order_items_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->string('order_number');
+
+            $table->integer('type_id');
+            $table->string('travel_option')->nullable();
             $table->integer('product_cate_id')->nullable();
             $table->integer('product_id');
             $table->float('product_price');
