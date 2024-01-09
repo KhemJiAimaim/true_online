dc866684592e5c0207d0c2bb3dc83dcdf86bd514
Nattapol <nattapol.surinkeaw@gmail.com>
2024-01-09 16:33:54 +0700

    b_update_email_order

---
+++ app/Mail/SendOrderDetail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendOrderDetail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $dataCustomer;
+    public function __construct($newOrder)
+    {
+        $this->dataCustomer = $newOrder;
+    }
+
+    public function build() 
+    {
+        return $this->subject('หมายเลขการสั่งซื้อเลขที่ '.$this->dataCustomer->order_number)
+            ->markdown('frontend.mail.formorder');
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: '',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
