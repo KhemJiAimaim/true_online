644bce008ee0d5cc903d3a9dba56e76fc1a7abd4
Nattapol <nattapol.surinkeaw@gmail.com>
2024-03-04 14:29:17 +0700

    update feedback

--- resources/views/frontend/pages/article/article-detail.blade.php
+++ resources/views/frontend/pages/article/article-detail.blade.php
@@ -17,7 +17,7 @@
     <h1 class="mt-8 mb-16 text-center 2xl:text-[2rem]  xl:text-[22px] text-[20px] font-medium">{{$post->title}}</h1>
 
     {{-- box content --}}
-    <div class="w-full mb-4 max-w-[1536px]">
+    <div class="w-full mb-4 max-w-[1536px]" id="article-content">
       {!! $post->content !!}
     </div>
   </div>
